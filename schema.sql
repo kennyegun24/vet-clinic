5240911afeed5dc9c72427485789ea67110ed700
BELINDA <kennyegun240@gmail.com>
2022-12-12 16:19:30 +0100

    Add new tables and linked tables together with foreign key

--- schema.sql
+++ schema.sql
@@ -10,3 +10,37 @@ CREATE TABLE animals (
 );
 
 ADD species VARCHAR(100);
+
+CREATE TABLE owners(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(150),
+  age INT
+);
+
+CREATE TABLE species(
+  id INT NOT NULL GENERATED ALWAYS as IDENTITY PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+ALTER TABLE animals
+  DROP COLUMN id;
+
+ALTER TABLE animals
+  ADD COLUMN id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+
+ALTER TABLE animals
+  DROP COLUMN species;
+
+ALTER TABLE animals
+  ADD COLUMN species_id INT,
+  ADD COLUMN owners_id INT;
+
+ALTER TABLE animals
+  ADD CONSTRAINT fk_owner
+  FOREIGN KEY (owner_id)
+  REFERENCES owners(id);
+
+ALTER TABLE animals
+  ADD CONSTRAINT fk_species
+  FOREIGN KEY (species_id)
+  REFERENCES species(id);
